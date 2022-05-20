10889573ecbadb4586269eeb29c13f7fe6189844
it21108372 <105626116+it21108372@users.noreply.github.com>
2022-05-20 18:34:53 +0530

    Update Doctor.cpp

--- Doctor.cpp
+++ Doctor.cpp
@@ -2,25 +2,31 @@
 #include<cstring>
 Doctor::Doctor()
 {
-docId = 0;
-strcpy(firstName, "");
-strcpy(lastName, "");
-strcpy(email, "");
-strcpy(pNo, "");
+    docId = 0;
+    strcpy(firstName, "");
+    strcpy(lastName, "");
+    strcpy(email, "");
+    strcpy(pNo, "");
 }
+
 Doctor::Doctor(int pdocId, const char pfirstName[], const char plastName[],
 const
+
 char pemail[], const char ppNo[])
 {
-docId = pdocId;
-strcpy(firstName, pfirstName);
-strcpy(lastName, plastName);
-strcpy(email, pemail);
-strcpy(pNo, ppNo);
+    docId = pdocId;
+    strcpy(firstName, pfirstName);
+    strcpy(lastName, plastName);
+    strcpy(email, pemail);
+    strcpy(pNo, ppNo);
 }
+    
 void Doctor::displayDetails()
-{
-}
+    {
+  
+    }
+
 Doctor::~Doctor()
-{
-}
+    {
+  
+    }
